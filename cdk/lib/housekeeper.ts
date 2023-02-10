e4c328e408e16ce24e56f41ec23b144dab41d4c1
Dina Hafez <dina.hafez@guardian.co.uk>
2023-02-10 13:33:28 +0000

    Created SNS topics and GuLambdaFunction
    Co-Authored-By: Akash Askoolum <akash1810@users.noreply.github.com>

--- cdk/lib/housekeeper.ts
+++ cdk/lib/housekeeper.ts
@@ -1,15 +1,36 @@
-import { join } from "path";
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
 import type { App } from "aws-cdk-lib";
-import { CfnInclude } from "aws-cdk-lib/cloudformation-include";
+import {GuLambdaFunction} from "@guardian/cdk/lib/constructs/lambda";
+import {Runtime} from "aws-cdk-lib/aws-lambda";
+import {SnsEventSource} from "aws-cdk-lib/aws-lambda-event-sources";
+import {Topic} from "aws-cdk-lib/aws-sns";
 
 export class Housekeeper extends GuStack {
+
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
-    const yamlTemplateFilePath = join(__dirname, "../..", "cfn.yaml");
-    new CfnInclude(this, "YamlTemplate", {
-      templateFile: yamlTemplateFilePath,
-    });
+
+    const bounceSNSTopic = new Topic(this, "BounceSNSTopic", {
+      displayName: "SNS topic alerted when AWS SES emails bounce (eg from Ophan Dashboard alerts, or Airflow)",
+      topicName: "ses-email-bounce-notifications-for-housekeeper"
+    })
+    this.overrideLogicalId(bounceSNSTopic, {logicalId: "BounceSNSTopic", reason: "We don't want AWS to delete the old topic and create a new one."})
+
+    const permanentEmailBounceTopic = new Topic(this, "PermanentEmailBounceTopic", {
+      displayName: "Threat to email-sending ability of Ophan's AWS account",
+    })
+    this.overrideLogicalId(permanentEmailBounceTopic, {logicalId: "PermanentEmailBounceTopic", reason: "We don't want AWS to delete the old topic and create a new one."})
+
+    const app = "housekeeper"
+
+    const lambda = new GuLambdaFunction(this, "housekeeper", {
+      app,
+      fileName: "ophan-dist/ophan/PROD/housekeeper/housekeeper.jar",
+      runtime: Runtime.JAVA_11,
+      handler: "housekeeper.Lambda::handler",
+      environment: {PermanentEmailBounceTopicArn: permanentEmailBounceTopic.topicArn}
+    })
+    lambda.addEventSource(new SnsEventSource(bounceSNSTopic));
   }
 }
