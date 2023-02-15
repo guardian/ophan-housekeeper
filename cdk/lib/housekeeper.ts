960e21ed8b47a4fbf4d72cfba2e96065cebfaa05
Dina Hafez <dina.hafez@guardian.co.uk>
2023-02-15 12:41:27 +0000

    Add CFN policies and used lint & prettier
    Co-Authored-By: Akash Askoolum <akash1810@users.noreply.github.com>

--- cdk/lib/housekeeper.ts
+++ cdk/lib/housekeeper.ts
@@ -1,36 +1,70 @@
-import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
-import { GuStack } from "@guardian/cdk/lib/constructs/core";
-import type { App } from "aws-cdk-lib";
-import {GuLambdaFunction} from "@guardian/cdk/lib/constructs/lambda";
-import {Runtime} from "aws-cdk-lib/aws-lambda";
-import {SnsEventSource} from "aws-cdk-lib/aws-lambda-event-sources";
-import {Topic} from "aws-cdk-lib/aws-sns";
+import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
+import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
+import type { App } from 'aws-cdk-lib';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { SnsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { Topic } from 'aws-cdk-lib/aws-sns';
 
 export class Housekeeper extends GuStack {
+	constructor(scope: App, id: string, props: GuStackProps) {
+		super(scope, id, props);
 
-  constructor(scope: App, id: string, props: GuStackProps) {
-    super(scope, id, props);
+		const bounceSNSTopic = new Topic(this, 'BounceSNSTopic', {
+			displayName:
+				'SNS topic alerted when AWS SES emails bounce (eg from Ophan Dashboard alerts, or Airflow)',
+			topicName: 'ses-email-bounce-notifications-for-housekeeper',
+		});
+		this.overrideLogicalId(bounceSNSTopic, {
+			logicalId: 'BounceSNSTopic',
+			reason: "We don't want AWS to delete the old topic and create a new one.",
+		});
 
-    const bounceSNSTopic = new Topic(this, "BounceSNSTopic", {
-      displayName: "SNS topic alerted when AWS SES emails bounce (eg from Ophan Dashboard alerts, or Airflow)",
-      topicName: "ses-email-bounce-notifications-for-housekeeper"
-    })
-    this.overrideLogicalId(bounceSNSTopic, {logicalId: "BounceSNSTopic", reason: "We don't want AWS to delete the old topic and create a new one."})
+		const permanentEmailBounceTopic = new Topic(
+			this,
+			'PermanentEmailBounceTopic',
+			{
+				displayName: "Threat to email-sending ability of Ophan's AWS account",
+			},
+		);
+		this.overrideLogicalId(permanentEmailBounceTopic, {
+			logicalId: 'PermanentEmailBounceTopic',
+			reason: "We don't want AWS to delete the old topic and create a new one.",
+		});
 
-    const permanentEmailBounceTopic = new Topic(this, "PermanentEmailBounceTopic", {
-      displayName: "Threat to email-sending ability of Ophan's AWS account",
-    })
-    this.overrideLogicalId(permanentEmailBounceTopic, {logicalId: "PermanentEmailBounceTopic", reason: "We don't want AWS to delete the old topic and create a new one."})
+		const app = 'housekeeper';
 
-    const app = "housekeeper"
+		const loggingPolicy = new PolicyStatement();
+		loggingPolicy.addResources('arn:aws:logs:*:*:*');
+		loggingPolicy.addActions(
+			'logs:CreateLogGroup',
+			'logs:CreateLogStream',
+			'logs:PutLogEvents',
+		);
 
-    const lambda = new GuLambdaFunction(this, "housekeeper", {
-      app,
-      fileName: "ophan-dist/ophan/PROD/housekeeper/housekeeper.jar",
-      runtime: Runtime.JAVA_11,
-      handler: "housekeeper.Lambda::handler",
-      environment: {PermanentEmailBounceTopicArn: permanentEmailBounceTopic.topicArn}
-    })
-    lambda.addEventSource(new SnsEventSource(bounceSNSTopic));
-  }
+		//TODO: change the codebase to use Dev-ophan-alerts table when testing locally or in CODE
+		const dynamodbPolicy = new PolicyStatement();
+		dynamodbPolicy.addResources(
+			`arn:aws:dynamodb:*:${this.account}:table/ophan-alerts`,
+			`arn:aws:dynamodb:*:${this.account}:table/DEV-ophan-alerts`,
+		);
+		dynamodbPolicy.addActions('dynamodb:Query', 'dynamodb:DeleteItem');
+
+		const lambda = new GuLambdaFunction(this, 'housekeeper', {
+			app,
+			fileName: 'housekeeper.jar',
+			description: 'Housekeeping for Ophan',
+			environment: {
+				PermanentEmailBounceTopicArn: permanentEmailBounceTopic.topicArn,
+			},
+			handler: 'housekeeper.Lambda::handler',
+			runtime: Runtime.JAVA_11,
+		});
+		lambda.addEventSource(new SnsEventSource(bounceSNSTopic));
+
+		permanentEmailBounceTopic.grantPublish(lambda);
+		lambda.addToRolePolicy(loggingPolicy);
+		lambda.addToRolePolicy(dynamodbPolicy);
+	}
 }
